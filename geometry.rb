2b60608f37f36cd9b3a1f797d2902be4d36a72b4
Fredrick Gorospe <gorospefredrick@gmail.com>
2020-09-15 17:25:05 +0800

    #{

--- geometry.rb
+++ geometry.rb
@@ -3,5 +3,5 @@ length = gets.to_f
 print "Please enter the width: "
 width = gets.to_f
 area = length * width
-puts "The area of the rectangle is " + area.to_s
+puts "The area of the rectangle is #{area}."
 
