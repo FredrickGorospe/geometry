04b3aa18c182deaf13bcb71b6109debfbbcf7eb5
Fredrick Gorospe <gorospefredrick@gmail.com>
2020-09-15 17:01:20 +0800

    Displays area

--- geometry.rb
+++ geometry.rb
@@ -2,4 +2,6 @@ print "Please enter the length: "
 length = gets.to_f
 print "Please enter the width: "
 width = gets.to_f
-area = length * width 
+area = length * width
+puts "The area of the rectangle is " + area.to_s
+
