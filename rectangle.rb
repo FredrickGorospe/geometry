b9e23929a7e500a778debe583c10da399af49433
Fredrick Gorospe <gorospefredrick@gmail.com>
2020-09-22 17:09:06 +0800

    Creates rectangle class

---
+++ rectangle.rb
@@ -0,0 +1,17 @@
+class Rectangle 
+  attr_accessor :length, :width
+
+  def initialize(length, width)
+    @length = length
+    @width = width
+  end
+
+  def area
+    length * width
+  end
+
+  def perimiter
+    2 * length + 2 * width
+  end
+end
+
